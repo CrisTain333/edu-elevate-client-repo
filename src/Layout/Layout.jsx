869e23ec69b337cf175507f352c984c9b3c98ea2
Cristain <mitashil99@gmail.com>
2022-10-25 16:09:15 +0400

    footer added

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../Shared/Header/Header'
+import Footer from '../Shared/Footer/Footer'
 const Layout = () => {
   return (
     <>
       <Header/>
       <Outlet></Outlet>
+      <Footer></Footer>
     </>
   )
 }
