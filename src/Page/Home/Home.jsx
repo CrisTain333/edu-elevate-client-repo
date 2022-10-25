0660a53e51c575e86957d7e35f88313ec01065ed
Cristain <mitashil99@gmail.com>
2022-10-25 16:55:34 +0400

    Home Page Completed

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
-
+import BrandLogo from '../../Image/brand.png'
 const Home = () => {
   return (
     <div>
       <section className="text-gray-600 body-font">
-  <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+  <div className="container mx-auto flex px-5 py-10 items-center justify-center flex-col">
     <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://images.pexels.com/photos/251225/pexels-photo-251225.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
     <div className="text-center lg:w-2/3 w-full">
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">It-World Is The Best Learning Platform Right Now</h1>
-      <p className="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
+      <p className="mb-8 leading-relaxed">“The more that you read, the more things you will know. The more that you learn, the more places you’ll go.”</p>
+      <p className='mb-3 leading-relaxed'>“In learning you will teach, and in teaching you will learn.”</p>
+    </div>
+  </div>
+</section>
+
+<section className="text-gray-600 body-font">
+  <div className="container mx-auto flex px-5  md:flex-row flex-col items-center">
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+      <img className="object-cover object-center rounded" alt="hero" src={BrandLogo}/>
+    </div>
+    <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">One of the most important areas we can develop as professionals is competence in accessing and sharing knowledge.
+      </h1>
+      <p className="mb-8 leading-relaxed">Online learning is not the next big thing, it is the now big thing</p>
     </div>
   </div>
 </section>
--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../../Image/brand.png'
+import logo from '../../Image/it.png'
+import { BsArrowBarRight } from "react-icons/bs";
+
 const Header = () => {
   return (
     <>
     <header className="text-gray-600 body-font">
-  <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
+  <div className="container mx-auto flex flex-wrap  flex-col md:flex-row items-center">
     <Link to='/' className="flex title-font font-medium items-center text-gray-900 mb-0 md:mb-0">
       <img className="w-20 h-20 text-white p-2 rounded-full" viewBox="0 0 24 24" src={logo} alt="" />
-      <span className="ml-2 text-xl ">It-Wolrd</span>
+      <span className=" text-2xl ">It-Wolrd</span>
     </Link>
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <Link  to='' className="mr-5 hover:text-gray-900">First Link</Link>
+      <Link  to='/' className="mr-5 hover:text-gray-900">Home</Link>
       <Link  to='' className="mr-5 hover:text-gray-900">Second Link</Link>
       <Link  to='' className="mr-5 hover:text-gray-900">Third Link</Link>
       <Link  to='' className="mr-5 hover:text-gray-900">Fourth Link</Link>
     </nav>
     <div className='md:ml-auto flex flex-wrap items-center justify-center'>
     <img className="w-8 h-8 mr-2  rounded-full" src="https://images.unsplash.com/photo-1618641986557-1ecd230959aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW5zdGFncmFtJTIwcHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" alt="user photo" 	/>
-    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
+    <button className="inline-flex items-center bg-gray-100 border-0 py-2 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Sing Out
+    <BsArrowBarRight className='ml-1'/>
     </button>
     </div>
   </div>
